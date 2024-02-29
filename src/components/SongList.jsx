4aadd4992143d8f96b315102da76c9da843c2e51
Qui Nguyen <qui@gmail.com>
2024-02-28 21:25:47 -0500

    first commit

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import NewSongForm from "./NewSongForm";
 const SongList = () => {
@@ -8,9 +8,20 @@ const SongList = () => {
     { title: "Lemon Tree", id: 3 },
     { title: "My Love", id: 4 },
   ]);
+
+  const [age, setAge] = useState(20);
+
   const addSong = (title) => {
     setSongs([...songs, { title, id: uuidv4() }]);
   };
+
+  useEffect(() => {
+    console.log("useEffect hook ran", songs);
+  }, [songs]);
+
+  useEffect(() => {
+    console.log("useEffect hook ran", age);
+  }, [age]);
 
   return (
     <div className="song-list">
@@ -20,6 +31,7 @@ const SongList = () => {
         ))}
       </ul>
       <NewSongForm addSong={addSong} />
+      <button onClick={() => setAge(age + 1)}>Add 1 to age: {age}</button>
     </div>
   );
 };
